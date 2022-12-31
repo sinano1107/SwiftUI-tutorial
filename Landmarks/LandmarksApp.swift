78ea202735f3e70d8a4240f4088ae54c7207cd35
sinano1107 <tftf161107@icloud.com>
2023-01-01 00:07:23 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 長政輝 on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
