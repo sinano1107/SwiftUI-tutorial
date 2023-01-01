7d6854f94fe5fb7d674be14bceb404b171b8603b
sinano1107 <tftf161107@icloud.com>
2023-01-01 21:36:53 +0900

    2-3

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-
+    
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
-
+            
             Spacer()
         }
     }
@@ -24,6 +24,10 @@ struct LandmarkRow: View {
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[0])
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
